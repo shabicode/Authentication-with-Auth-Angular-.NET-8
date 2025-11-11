148d3ea1726d65e77f09c4f47f83548c0583580f
shabioks <monicaab.mtz95@gmail.com>
2025-11-10 23:54:02 -0600

    create login component

--- Web-SPA/auth-demo/src/app/app.ts
+++ Web-SPA/auth-demo/src/app/app.ts
@@ -1,12 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('auth-demo');
+ 
 }
