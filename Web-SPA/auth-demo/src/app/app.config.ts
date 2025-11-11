28891ca85db2a16ad46c0e866711ecaa2785e4d7
shabioks <monicaab.mtz95@gmail.com>
2025-11-10 23:12:55 -0600

    set up app.config

--- Web-SPA/auth-demo/src/app/app.config.ts
+++ Web-SPA/auth-demo/src/app/app.config.ts
@@ -2,11 +2,36 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { AuthHttpInterceptor, provideAuth0 } from '@auth0/auth0-angular';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+  providers: [    
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthHttpInterceptor,
+      multi: true
+    },
+    provideAuth0({
+      domain: environment.auth.domain,
+      clientId: environment.auth.clientId,
+      authorizationParams: environment.auth.authorizationParams,
+      httpInterceptor: {
+        allowedList: [
+          {
+            uri: `${environment.apiBaseUrl}/*`,
+            tokenOptions: {
+              authorizationParams: {
+                audience: environment.auth.authorizationParams.audience,
+                scope: environment.auth.authorizationParams.scope
+              }
+            }
+          }
+        ]
+      }
+    })
   ]
 };
